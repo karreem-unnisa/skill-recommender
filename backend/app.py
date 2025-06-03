d27d5ea87759e4c6b1f0679a4564671625bb8812
Kareemunnisa <mailkareemunnisa@gmail.com>
2025-06-03 21:26:28 +0530

    app change

--- backend/app.py
+++ backend/app.py
@@ -13,4 +13,7 @@ app.register_blueprint(skill_routes, url_prefix='/api')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
